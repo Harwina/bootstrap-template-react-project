d5c3e29a5f1e14acd2b43ba5ddc37113bdd668d1
Harwina <164463440+Harwina@users.noreply.github.com>
2024-10-13 11:49:24 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,19 @@
+import Header from "./Component/Header"
+import Banner from "./Component/Banner"
+import Catagory from "./Component/Catagory"
+import PopularProduct from "./Component/Popular-product"
+
+function App() {
+  
+
+  return (
+    <>
+      <Header/>
+      <Banner/>
+      <Catagory/>
+      <PopularProduct/>
+    </>
+  )
+}
+
+export default App
---
+++ Product-banner.jsx
@@ -0,0 +1,17 @@
+const ProductBanner = () => {
+    return (
+        <div className="product-banner-container">
+            <div className="image-baneer">
+                <img src="assets/img/product-banner/2.jpg" alt="" />
+                <div className="banner-contain">
+                    <h5>Fresh Snacks &amp; Sweets</h5>
+                    <p><span class="percent">20%</span> Off <span class="text">on first order</span>
+                    </p>
+                </div>
+                <div className="cr-product-banner-buttons">
+        <a href="shop-left-sidebar.html" className="cr-button">shop now</a>
+    </div>
+            </div>
+        </div>
+    )
+}
